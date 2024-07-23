cc49d5d8bea174ffaabc006e81fcaab6514d5eb1
iannovais <iandosreisnovais@gmail.com>
2024-07-23 16:23:24 -0300

    Finalizado cadastro de calçados

--- public/cadastro_calcados/back-end/js/cadastro_calcados.js
+++ public/cadastro_calcados/back-end/js/cadastro_calcados.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
+    function carregarOpcoesSelect(url, selectId) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function (data) {
+                $(selectId).empty();
+                $(selectId).append('<option value="">Selecione...</option>');
+                data.forEach(function (item) {
+                    $(selectId).append('<option value="' + item.id + '">' + item.nome + '</option>');
+                });
+            },
+            error: function () {
+                console.error('Erro ao carregar opções para ' + selectId);
+            }
+        });
+    }
+
+    carregarOpcoesSelect('/tintas', '#select-tinta');
+    carregarOpcoesSelect('/materiais', '#select-material');
+    carregarOpcoesSelect('/cadarcos', '#select-cadarco');
+    carregarOpcoesSelect('/solados', '#select-solado');
+    carregarOpcoesSelect('/adesivos', '#select-adesivo');
+    carregarOpcoesSelect('/tipo-de-tintas', '#select-tipo-de-tinta');
+
     $('#cadastrar-calcado').submit(function (e) {
         e.preventDefault();
 
         var formData = $(this).serialize();
-        console.log(formData);
 
         $.ajax({
             type: 'POST',
@@ -16,7 +39,7 @@ $(document).ready(function () {
                     title: "Enviado com sucesso",
                     showConfirmButton: false,
                     timer: 1500
-                }).then(function() {
+                }).then(function () {
                     $('#cadastrar-calcado')[0].reset();
                 });
             },
--- routes/calcados.js
+++ routes/calcados.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const { connection2 } = require('../public/chat/js/db.js');
+const moment = require('moment');
 
 // Rota POST para inserir uma demanda de calçado no banco de dados ProductEase
 router.post('/calcado', (req, res) => {
-  const { nome, espe_linha, espacamento_da_costura, temp_equi, cor_linha, tam_costura, temp_sec, reg_equip, img_calcado, id_tinta, id_material, id_cadarco, id_solado, id_adesivo, id_tip_tinta, data } = req.body;
+  const { nome, espe_linha, espacamento_da_costura, temp_equi, cor_linha, tam_costura, temp_sec, reg_equip, img_calcado, id_tinta, id_material, id_cadarco, id_solado, id_adesivo, id_tip_tinta } = req.body;
+
+  const data = moment().format('YYYY-MM-DD');
 
   const query = `INSERT INTO calcado (nome, espacamento_da_costura, espe_linha, temp_equi, cor_linha, tam_costura, temp_sec, reg_equip, img_calcado, id_tinta, id_material, id_cadarco, id_solado, id_adesivo, id_tip_tinta, data) 
                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   connection2.query(query, [nome, espacamento_da_costura, espe_linha, temp_equi, cor_linha, tam_costura, temp_sec, reg_equip, img_calcado, id_tinta, id_material, id_cadarco, id_solado, id_adesivo, id_tip_tinta, data], (err) => {
-      if (err) {
-          console.error('Erro ao inserir calcado no banco de dados:', err);
-          res.status(500).send('Erro ao enviar calcado');
-          return;
-      }
-      res.status(201).send('Calcado enviado com sucesso');
+    if (err) {
+      console.error('Erro ao inserir calcado no banco de dados:', err);
+      res.status(500).send('Erro ao enviar calcado');
+      return;
+    }
+    res.status(201).send('Calcado enviado com sucesso');
   });
 });
 
 router.get('/info_produtos/:id', (req, res) => {
-    const id = req.params.id;
-  
-    const query = `
+  const id = req.params.id;
+
+  const query = `
       SELECT 
         material.nome as nome,
         tipo_tinta.nome as tipotinta,
@@ -45,21 +48,97 @@ router.get('/info_produtos/:id', (req, res) => {
       WHERE 
         calcado.id = ?
     `;
-  
-    connection2.query(query, [id], (err, results) => {
-      if (err) {
-        console.error('Erro ao buscar dados:', err);
-        res.status(500).send({ message: 'Erro ao buscar dados' });
+
+  connection2.query(query, [id], (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar dados:', err);
+      res.status(500).send({ message: 'Erro ao buscar dados' });
+    } else {
+      if (results.length === 0) {
+        res.json(false);
       } else {
-        if (results.length === 0) {
-          res.json(false);
-        } else {
-          res.json(results);
-        }
+        res.json(results);
       }
-    });
+    }
+  });
 });
 
+// Rota GET para obter os adesivos do banco de dados ProductEase
+router.get('/adesivos', (req, res) => {
+  const query = 'SELECT * FROM adesivo';
+  connection2.query(query, (err, results) => {
+      if (err) {
+          console.error('Erro ao buscar os adesivos:', err);
+          res.status(500).send('Erro ao buscar adesivos');
+          return;
+      }
+      res.json(results);
+  });
+});
 
+// Rota GET para obter os cardaços do banco de dados ProductEase
+router.get('/cadarcos', (req, res) => {
+  const query = 'SELECT * FROM cadarco';
+  connection2.query(query, (err, results) => {
+      if (err) {
+          console.error('Erro ao buscar os cadarços:', err);
+          res.status(500).send('Erro ao buscar cadarços');
+          return;
+      }
+      res.json(results);
+  });
+});
+
+// Rota GET para obter as tintas do banco de dados ProductEase
+router.get('/tintas', (req, res) => {
+  const query = 'SELECT * FROM tinta';
+  connection2.query(query, (err, results) => {
+      if (err) {
+          console.error('Erro ao buscar as tintas:', err);
+          res.status(500).send('Erro ao buscar tintas');
+          return;
+      }
+      res.json(results);
+  });
+});
+
+// Rota GET para obter os materiais do banco de dados ProductEase
+router.get('/materiais', (req, res) => {
+  const query = 'SELECT * FROM material';
+  connection2.query(query, (err, results) => {
+      if (err) {
+          console.error('Erro ao buscar os materiais:', err);
+          res.status(500).send('Erro ao buscar materiais');
+          return;
+      }
+      res.json(results);
+  });
+});
+
+// Rota GET para obter os solados do banco de dados ProductEase
+router.get('/solados', (req, res) => {
+  const query = 'SELECT * FROM solado';
+  connection2.query(query, (err, results) => {
+      if (err) {
+          console.error('Erro ao buscar os solados:', err);
+          res.status(500).send('Erro ao buscar solados');
+          return;
+      }
+      res.json(results);
+  });
+});
+
+// Rota GET para obter os tipos de tinta do banco de dados ProductEase
+router.get('/tipo-de-tintas', (req, res) => {
+  const query = 'SELECT * FROM tipo_tinta';
+  connection2.query(query, (err, results) => {
+      if (err) {
+          console.error('Erro ao buscar os tipos de tinta:', err);
+          res.status(500).send('Erro ao buscar tipos de tinta');
+          return;
+      }
+      res.json(results);
+  });
+});
 
 module.exports = router;
