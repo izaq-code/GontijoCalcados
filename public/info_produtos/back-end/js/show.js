154815ba55877aa96bc71106c5b418125007249b
cout"izaq <122700689+izaq-code@users.noreply.github.com>
2024-07-26 21:31:38 -0300

    upando tresd

--- public/info_produtos/back-end/js/show.js
+++ public/info_produtos/back-end/js/show.js
@@ -1,96 +1,46 @@
-var amostar = localStorage.getItem('id');
+$(document).ready(function() {
+    var amostar = localStorage.getItem('id');
 
-
-
-    $(document).ready(function() {
- 
-        $.ajax({
-            type: 'GET',
-            url: '/info_produtos',
-            data: {
-                id : amostar
-            }, // Envia formData como dados da requisição
-            dataType: 'json',
-            success: function(data) {
-                console.log(data);
-                if (data) {
-                    exibir(data);
-                } else {
-                    alert('Nenhum modelo encontrado.');
-                }
-            },
-            error: function(err) {
-                console.error('Erro na requisição AJAX:', err);
-                alert('Erro ao buscar dados');
+    $.ajax({
+        type: 'GET',
+        url: '/info_produtos',
+        data: { id: amostar },
+        dataType: 'json',
+        success: function(data) {
+            if (data && data.length > 0) {
+                exibir(data);
+            } else {
+                alert('Nenhum modelo encontrado.');
             }
-        });
+        },
+        error: function(err) {
+            console.error('Erro na requisição AJAX:', err);
+            alert('Erro ao buscar dados');
+        }
     });
+});
 
+function exibir(data) {
+    var modelViewer = document.getElementById('modelViewer');
+    var item = data[0];
 
-function exibir(data){
-    console.log('me chamaram');
-    
-    a = $('#nome');
+    // Ensure the path is correct
+    var modelPath = item['modelo3d'].startsWith('http') ? item['modelo3d'] : new URL(item['modelo3d'], window.location.origin).href;
+    console.log('Modelo 3D Path:', modelPath);
 
-    b = $('#tipo_tinta');
+    // Update the model viewer
+    modelViewer.setAttribute('src', modelPath);
 
-    c = $('#espe_linha');
-
-    d = $('#nome_solado');
-
-    q = $('#img');
-
-    x = $('#nome_cadarco');
-
-    f = $('#tempo_calcado');
-
-    g = $('#nome_tinta');
-
-    h = $('#corlinhacalcado');
-
-    i = $('#temcosturacalcado');
-
-    j = $('#temp_sec');
-
-
-    
-    data.forEach(function(e){
-
-        var table = $('<img>').attr('src', e['img']);
-        q.append(table);
-
-        var table = $('<p>').text(e['nome']);//"<p>" + e['nome'] + "</p>"
-        a.append(table); 
-
-        var table = $('<p>').text(e['tipotinta']);
-        b.append(table); 
-
-        var table = $('<p>').text(e['espelinha']);
-        c.append(table);
-
-        var table = $('<p>').text(e['nomesolado']);
-        d.append(table);
-
-        var table = $('<p>').text(e['nomecadarco']);
-        x.append(table);
-
-        var table = $('<p>').text(e['tempocalcado']);
-        f.append(table);
-
-        var table = $('<p>').text(e['nometinta']);
-        g.append(table);
-
-        var table = $('<p>').text(e['corlinhacalcado']);
-        h.append(table);
-
-        var table = $('<p>').text(e['temcosturacalcado']);
-        i.append(table);
-
-        var table = $('<p>').text(e['tempsec']);
-        j.append(table);
-
-
-    });
-
+    // Update other information
+    $('#img').html($('<img>').attr('src', item['img']));
+    $('#nome').text('Nome: ' + item['nome']);
+    $('#tipo_tinta').text('Tipo de Tinta: ' + item['tipotinta']);
+    $('#espe_linha').text('Espessura de Linha: ' + item['espelinha']);
+    $('#nome_solado').text('Nome do Solado: ' + item['nomesolado']);
+    $('#nome_cadarco').text('Nome do Cadarço: ' + item['nomecadarco']);
+    $('#tempo_calcado').text('Tempo de Calçado: ' + item['tempocalcado']);
+    $('#nome_tinta').text('Nome da Tinta: ' + item['nometinta']);
+    $('#corlinhacalcado').text('Cor da Linha do Calçado: ' + item['corlinhacalcado']);
+    $('#temcosturacalcado').text('Tempo de Costura do Calçado: ' + item['temcosturacalcado']);
+    $('#temp_sec').text('Tempo de Secagem: ' + item['tempsec']);
 }
-
