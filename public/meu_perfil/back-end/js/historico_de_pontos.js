867c4723023f06070563e5fae928d9ce5d5b351c
Eduarda Vieira <eduarda.vieira.goncalves7@gmail.com>
2024-08-08 13:19:07 -0300

    concertado

--- public/meu_perfil/back-end/js/historico_de_pontos.js
+++ public/meu_perfil/back-end/js/historico_de_pontos.js
@@ -118,68 +118,70 @@ function listarFuncionarios() {
 
     container.innerHTML = cabeçalho;
 
-    var data_ini = document.getElementById('data_ini').value;
-    var data_fim = document.getElementById('data_fim').value;
+    function atualizarHistorico() {
+        var data_ini = document.getElementById('data_ini').value;
+        var data_fim = document.getElementById('data_fim').value;
 
-    console.log("Data Início:", data_ini);
-    console.log("Data Fim:", data_fim);
+        console.log("Data Início:", data_ini);
+        console.log("Data Fim:", data_fim);
 
-    $.ajax({
-        url: '/historico',
-        type: 'GET',
-        dataType: 'json',
-        data: {
-            id: idUsuarioLogado,
-            data_ini: data_ini,
-            data_fim: data_fim
-        },
-        success: function (historico) {
-            console.log("Historico:", historico);
+        $.ajax({
+            url: '/historico',
+            type: 'GET',
+            dataType: 'json',
+            data: {
+                id: idUsuarioLogado,
+                data_ini: data_ini,
+                data_fim: data_fim
+            },
+            success: function (historico) {
+                console.log("Historico:", historico);
 
-            var tbody = document.getElementById('historico');
+                var tbody = document.getElementById('historico');
 
-            if (Array.isArray(historico) && historico.length > 0) {
-                historico.forEach(item => {
-                    var ponto_inicial = item.ponto_inicial || '00:00:00';
-                    var ponto_final = item.ponto_final || '00:00:00';
-                    var banco = item.banco || '00:00:00';
-                    var data = item.data || '00-00-0000';
-                    var horas_trabalhadas = item.horas_trabalho || '00:00:00';
-                    var banco_de_horas_anterior = item.banco_de_horas_anterior || '00:00:00';
+                tbody.innerHTML = ''; 
 
-                    var row = `
-                        <tr class="linha-abaixo">
-                            <td>${data}</td>
-                            <td>${ponto_inicial} - ${ponto_final}</td>
-                            <td>${horas_trabalhadas}</td>
-                            <td>${banco_de_horas_anterior}</td>
-                            <td>${banco}</td>
-                        </tr>
-                     
-                    `;
+                if (Array.isArray(historico) && historico.length > 0) {
+                    historico.forEach(item => {
+                        var ponto_inicial = item.ponto_inicial || '00:00:00';
+                        var ponto_final = item.ponto_final || '00:00:00';
+                        var banco = item.banco || '00:00:00';
+                        var data = item.data || '00-00-0000';
+                        var horas_trabalhadas = item.horas_trabalho || '00:00:00';
+                        var banco_de_horas_anterior = item.banco_de_horas_anterior || '00:00:00';
 
-                    tbody.innerHTML += row;
-                });
+                        var row = `
+                            <tr class="linha-abaixo">
+                                <td>${data}</td>
+                                <td>${ponto_inicial} - ${ponto_final}</td>
+                                <td>${horas_trabalhadas}</td>
+                                <td>${banco_de_horas_anterior}</td>
+                                <td>${banco}</td>
+                            </tr>
+                        `;
 
-            } else {
-                container.innerHTML += '<p>Histórico não encontrado.</p>';
+                        tbody.innerHTML += row;
+                    });
+                } else {
+                    tbody.innerHTML = '<tr><td colspan="5">Histórico não encontrado.</td></tr>';
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('Erro ao listar o histórico:', textStatus, errorThrown);
+                container.innerHTML += '<p>Erro ao carregar histórico.</p>';
             }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Erro ao listar o histórico:', textStatus, errorThrown);
-            container.innerHTML = '<p>Erro ao carregar histórico.</p>';
-        }
-    });
-
-        document.getElementById('filtrar').addEventListener('click', function() {
-            listarFuncionarios();
         });
+    }
 
+    document.getElementById('filtrar').addEventListener('click', atualizarHistorico);
     document.getElementById('mostrar-todos').addEventListener('click', function() {
         document.getElementById('data_ini').value = '';
         document.getElementById('data_fim').value = '';
-        listarFuncionarios();
+        atualizarHistorico();
     });
+
+   
+    atualizarHistorico();
 }
 
 carregarInformacoes(function() {
