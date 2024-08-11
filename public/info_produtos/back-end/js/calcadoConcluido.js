2b31ad6e18521002ceadf1427f41e81363013bfa
iannovais <iandosreisnovais@gmail.com>
2024-08-11 20:18:32 -0300

    Criado botão de finalizar demanda

--- public/info_produtos/back-end/js/calcadoConcluido.js
+++ public/info_produtos/back-end/js/calcadoConcluido.js
@@ -1,4 +1,42 @@
-const idCalcado = localStorage.getItem('id');
+$('#calcadoCad').click(function(){
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        },
+
+    });
+
+    swalWithBootstrapButtons.fire({
+        title: "Tem certeza?",
+        text: "Você não poderá reverter isso!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sim, enviar!",
+        cancelButtonText: "Não, cancelar!",
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire({
+                title: 'Criado com Sucesso!',
+                text: 'Você será redirecionado em breve.',
+                icon: 'success',
+                timer: 2000, 
+                showConfirmButton: false
+            }).then(() => {
+                // Função de conclusão do processo
+                calcadoConcluido();
+                window.location.href = 'http://localhost:3000/info_produtos/front-end/html/selecao.html';
+            });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire({
+                title: "Cancelado",
+                text: "A ação foi cancelada",
+                icon: "error"
+            });
+        }
+    });
+})
 
 function calcadoConcluido () {
     $.ajax({
